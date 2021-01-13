fadeb6f4834e37c090ba4edf598828faa5dfbedc
denysnovoa <dnovoa20@gmail.com>
2021-01-13 14:17:51 +0100

    first commit

---
+++ app/src/main/java/com/dnovoa/mediamanager/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dnovoa.mediamanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dnovoa.mediamanager.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
